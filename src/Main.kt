eb1cc3788c60ac5565acd67114f14e609b0c249c
Bernardo <bernardojspa@gmail.com>
2018-12-20 19:58:07 +0000

    tawea

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,5 @@
 import java.util.Random
-
+//asd aa d
 fun main(args: Array<String>) {
     println()
     println("*** BATALHA NAVAL ***")
@@ -9,9 +9,9 @@ fun main(args: Array<String>) {
     println()
     var opcao: Int?
     var tabuleiroSize: Int?
-    var nomeNaoFormatado :String
-    var jogador: String =""
-    var tabuleiro: String =""
+    var nomeNaoFormatado: String
+    var jogador: String = ""
+    var tabuleiro: String = ""
 
 
     do {
@@ -36,28 +36,28 @@ fun main(args: Array<String>) {
                 //valida nome
                 do {
                     nomeNaoFormatado = readLine().orEmpty()
-                    var jogadorCaracteres =0
+                    var jogadorCaracteres = 0
                     for (caracter in nomeNaoFormatado) {
                         if (caracter != ' ') {
                             jogadorCaracteres++
                         }
                     }
-                    if(nomeNaoFormatado.isEmpty() || jogadorCaracteres == 0 ){
+                    if (nomeNaoFormatado.isEmpty() || jogadorCaracteres == 0) {
                         println(">>> Nome inválido, tente novamente")
                     }
-                }while(nomeNaoFormatado.isEmpty() || jogadorCaracteres == 0 )
+                } while (nomeNaoFormatado.isEmpty() || jogadorCaracteres == 0)
 
                 //formata nome
-                var primeiroCaracter: Int =0
-                var nomeOk=false
-                for (caracter in 0..nomeNaoFormatado.length-1 ) {
+                var primeiroCaracter: Int = 0
+                var nomeOk = false
+                for (caracter in 0..nomeNaoFormatado.length - 1) {
                     if (nomeNaoFormatado[caracter] != ' ') {
                         primeiroCaracter++
                     }
-                    if(primeiroCaracter>=1&&nomeNaoFormatado[caracter] == ' '){
-                        nomeOk=true
+                    if (primeiroCaracter >= 1 && nomeNaoFormatado[caracter] == ' ') {
+                        nomeOk = true
                     }
-                    if(nomeOk==false) {
+                    if (nomeOk == false) {
                         if (primeiroCaracter == 1) {
                             jogador = jogador + nomeNaoFormatado[caracter].toUpperCase().toString()
                         } else {
@@ -96,7 +96,7 @@ fun main(args: Array<String>) {
                 }
                 println()
                 //linha cordenadas 10
-                numespaco =1
+                numespaco = 1
                 for (linha in 1..1) {
                     for (coluna in 1..tabuleiroSize) {
                         if (coluna % 10 == 0) {
@@ -112,23 +112,23 @@ fun main(args: Array<String>) {
                 //inserir mavios
                 println("$jogador, introduz a posição para 1 navio-tanque:")
 
-                var navioTanque: Int?= readLine()!!.toIntOrNull()
-                var tatuleiroTemp:String =""
+                var navioTanque: Int? = readLine()!!.toIntOrNull()
+                var tatuleiroTemp: String = ""
                 //valida navio-tanque
 
-                while (navioTanque == null || navioTanque>tabuleiroSize-3 || navioTanque<0 ) {
+                while (navioTanque == null || navioTanque > tabuleiroSize - 3 || navioTanque < 0) {
                     println(">>> Possicao invalida inválido, tente novamente")
-                    navioTanque= readLine()!!.toIntOrNull()
+                    navioTanque = readLine()!!.toIntOrNull()
                 }
 
-                for(esp in 1..tabuleiroSize-1){
-                    if(esp ==navioTanque ||esp ==navioTanque+1||esp ==navioTanque+2 ){
+                for (esp in 1..tabuleiroSize - 1) {
+                    if (esp == navioTanque || esp == navioTanque + 1 || esp == navioTanque + 2) {
                         tatuleiroTemp = "${tatuleiroTemp}3"
-                    }else{
+                    } else {
                         tatuleiroTemp = "${tatuleiroTemp}~"
                     }
                 }
-                tabuleiro =tatuleiroTemp
+                tabuleiro = tatuleiroTemp
 
                 println("$tabuleiro")
 
